962f914855e17c3832a5c97f290a4896f96c741b
boscha1 <bosch.anthony7@gmail.com>
2023-04-03 15:57:50 -0400

    fixing TeamPage

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {getTeamById} from "../services/TeamService"
-import Team from "../components/team/Team";
+import { Box, Heading, Text, VStack, HStack } from "@chakra-ui/react";
 
 const TeamPage = () => {
     const {id} = useParams()
@@ -9,9 +9,9 @@ const TeamPage = () => {
         id: null,
         name: '',
         establishedDate: '',
-        stadium: '',
         fightSong: '',
         mascot: '',
+        stadiums: [],
         location: {
             city: '',
             state: '',
@@ -25,7 +25,7 @@ const TeamPage = () => {
             losses: -1,
             ties: -1
         },
-        colorIds: []
+        colors: []
     })
 
     useEffect(() => {
@@ -38,10 +38,66 @@ const TeamPage = () => {
     }, [id])
 
     return (
-        <div>
-            <Team team={team}/>
-        </div>
-    );
+        <Box>
+          <Heading size="2xl" textAlign="center">
+            {team.prefix} {team.name}
+          </Heading>
+          <Box m={4}>
+            <VStack spacing={4} align="flex-start">
+              <Text fontSize="lg">
+                <strong>Location:</strong> {team.location.city}, {team.location.state}
+              </Text>
+              <Text fontSize="lg">
+                <strong>Established:</strong> {new Date(team.establishedDate).getFullYear()}
+              </Text>
+              <Text fontSize="lg">
+                <strong>Fight Song:</strong> {team.fightSong}
+              </Text>
+              <Text fontSize="lg">
+                <strong>Mascot:</strong> {team.mascot}
+              </Text>
+              <Box>
+                <Text fontSize="lg" mb={2}>
+                  <strong>Stadiums:</strong>
+                </Text>
+                <VStack align="flex-start" spacing={2}>
+                  {team.stadiums.map((stadium) => (
+                    <Box key={stadium.id}>
+                      <Text>
+                        {stadium.name} ({new Date(stadium.fromDate).getFullYear()} -{" "}
+                        {stadium.toDate ? new Date(stadium.toDate).getFullYear() : "present"})
+                      </Text>
+                    </Box>
+                  ))}
+                </VStack>
+              </Box>
+              <Box>
+                <Text fontSize="lg" mb={2}>
+                  <strong>Head Coach:</strong>
+                </Text>
+                <HStack>
+                  <Text>
+                    {team.headCoach.firstName} {team.headCoach.lastName}
+                  </Text>
+                  <Text>
+                    ({team.headCoach.wins}-{team.headCoach.losses}-{team.headCoach.ties})
+                  </Text>
+                </HStack>
+              </Box>
+              <Box>
+                <Text fontSize="lg" mb={2}>
+                  <strong>Colors:</strong>
+                </Text>
+                <HStack spacing={4}>
+                  {team.colors.map((color) => (
+                    <Box key={color.id} w="50px" h="50px" borderRadius="50%" bg={color.hexCode}></Box>
+                  ))}
+                </HStack>
+              </Box>
+            </VStack>
+          </Box>
+        </Box>
+      )
 };
 
 export default TeamPage;
